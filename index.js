e5c03357e1ee31d78263f16d0c7b9a1b4175e18f
jubayer1418 <skjubayer1418@gmail.com>
2023-06-08 20:38:12 +0600

    add user collection

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const port = process.env.PORT || 5000;
 //middlewere
 app.use(cors());
 app.use(express.json());
-
+//jwt verifu
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dxmnpk6.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,7 +38,52 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    const usersCollection = client.db("martialDb").collection("users");
+    const classesCollection = client.db("martialDb").collection("classes");
+    const instructorsCollection = client
+      .db("martialDb")
+      .collection("instructors");
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    //verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden message" });
+      }
+      next();
+    };
+    //users relades api
+    app.get("users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const alreadyUser = await usersCollection.findOne(query);
+      if (alreadyUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.post("/addclass", async (req, res) => {
+      const newClass = req.body;
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result);
+    });
+    app.get("/addclass", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
